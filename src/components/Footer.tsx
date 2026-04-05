416777e2c32c5626a8ef9e6dc91726d964548e55
Lina Maria Caicedo <linamacaicedo@gmail.com>
2026-04-05 12:01:06 -0500

    start of footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import '../styles/footer.css'
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer-brand">
+          <div className="footer-logo">UHome</div>
+          <p>Find your place, fit your lifestyle.</p>
+        </div>
+
+        <div className="footer-col">
+          <h4>Quick Links</h4>
+          <a href="#">Browse listings</a>
+          <a href="#">How it Works</a>
+          <a href="#">Compare Options</a>
+          <a href="#">FAQs</a>
+        </div>
+  )
+}
+
+export default Footer
