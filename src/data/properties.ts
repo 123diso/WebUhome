562c0ba1c92c25be4cb4fa6b6b45c02119406ab8
Lina Maria Caicedo <linamacaicedo@gmail.com>
2026-04-04 21:27:19 -0500

    start propertiests sim backend

---
+++ src/data/properties.ts
@@ -0,0 +1,11 @@
+const property = {
+  id: 1,
+  title: 'Cozy Shared House',
+  location: '8 min from Campus by car',
+  rating: 4.3,
+  roomType: 'Private room',
+  description: [
+    'Looking for a comfortable and well-located place to live? We have a private room available in a peaceful, student-friendly house just minutes away from Harvard University.',
+    'The room is for single occupancy only. You will not have to share it. The house currently has 5 residents and offers a welcoming, respectful, and friendly environment.',
+  ],
+}
