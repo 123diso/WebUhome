6a52866d39ec0baa170a7f0f5e308d354dcf73d6
123diso <127856684+123diso@users.noreply.github.com>
2026-04-05 10:04:14 -0500

    feat: add start page and route

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Start from './pages/Start';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/start" element={<Start />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Start.tsx
@@ -0,0 +1,16 @@
+import Navbar from '../components/Navbar';
+import Housinglist from '../components/HousingList';
+import Footer from '../components/Footer';
+import '../styles/start.css';
+
+function Start() {
+  return (
+    <>
+      <Navbar />
+      <Housinglist />
+      <Footer />
+    </>
+  );
+}
+
+export default Start;
