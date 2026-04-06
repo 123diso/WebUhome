4cc1fdf9394998b54a972f9d84faabf94d094b92
Lina Maria Caicedo <linamacaicedo@gmail.com>
2026-04-05 19:58:18 -0500

    all done thank to goddd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,10 +12,12 @@ function Navbar() {
       <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/">Search</Link>
-        <Link to="/profile.tsx" className="menu-btn" aria-label="Go to profile">
+        <Link to="/profile" className="menu-btn" aria-label="Go to profile">
           <FaRegUserCircle />
         </Link>
       </nav>
     </header>
   )
 }
+
+export default Navbar
